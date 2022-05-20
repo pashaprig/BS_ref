6f62b513315ec79b5b743d697db81fb96f873631
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-05-20 17:10:50 +0300

    Fonts + icon

--- gulpfile.js
+++ gulpfile.js
@@ -103,7 +103,7 @@ exports.sprite = sprite;
 
 const copy = (done) => {
   gulp.src([
-    "source/fonts/*.{woff2,woff}",
+    "source/fonts/**/*.{woff2,woff,ttf}",
     "source/*.ico",
     "source/img/**/*.svg",
     "!source/img/icons/*.svg",
